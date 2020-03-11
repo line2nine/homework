a358e6c63e9442bc88bcb8c766852b2b34988860
line2nine <hailinh203@gmail.com>
2020-03-11 08:52:24 +0700

    Add files

--- BuffaloAndGrass/js/my-js.js
+++ BuffaloAndGrass/js/my-js.js
@@ -1,6 +1,6 @@
 let trauDung, trauNam, trauGia;
-for (trauDung = 1; trauDung <= 20; trauDung++) {
-    for (trauNam = 1; trauNam <= 33; trauNam++) {
+for (trauDung = 0; trauDung <= 20; trauDung++) {
+    for (trauNam = 0; trauNam <= 33; trauNam++) {
         trauGia = 100 - (trauDung + trauNam);
         if (5 * trauDung + 3 * trauNam + trauGia / 3 === 100 && trauGia % 3 === 0) {
             document.write("Có trường hợp sau: " + "<br>" + "Trâu đứng bằng : " + trauDung + "<br>" + "Trâu nằm bằng :" + trauNam + "<br>" + "Trâu già bằng :" + trauGia + "<br><br>");
